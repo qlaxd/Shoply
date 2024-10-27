97cfb723c668f95ae21325bf0cc3fd7cf88ef190
levdev0 <bsze3lajlev@vasvari.org>
2024-10-27 16:53:16 +0100

    login/signup page created

--- Forráskód/Web/web-app/src/components/LoginSignup/LoginSignup.jsx
+++ Forráskód/Web/web-app/src/components/LoginSignup/LoginSignup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './LoginSignup.css'
 
 // importing png icons
@@ -6,19 +6,22 @@ import user_icon from '../assets/user.png'
 import email_icon from '../assets/email.png'
 import password_icon from '../assets/padlock.png'
 
-// 
+// LoginSignup komponens
 const LoginSignup = () => {
+
+  const [action,setAction] = useState('Sign Up') // melyik gomb az aktív - action változó
+
   return (
     <div className='container'>
         <div className='header'>
-          <div className='text'></div>
+          <div className='text'>{action}</div>
           <div className='underline'></div>
         </div>
         <div className='inputs'>
-          <div className='input'>
+          {action === "Log In" ? <div></div>:<div className='input'>
             <img src={user_icon} alt='user' />
             <input type='text' placeholder='Username' />
-          </div>
+          </div>}
           <div className='input'>
             <img src={email_icon} alt='email' />
             <input type='email' placeholder='Email' />
@@ -27,6 +30,12 @@ const LoginSignup = () => {
             <img src={password_icon} alt='password' />
             <input type='password' placeholder='Password' />
           </div>
+        </div>
+        {/* ha a Sign Up action az aktív akkor nincs forgot password, ha Log In akkor van */}
+        {action === "Sign Up" ? <div></div>:<div className='forgot-password'>Forgot Password? <span>Click Here</span></div>} 
+        <div className='submit-container'>
+          <div className={action === "Sign Up" ? "submit grey" : "submit"} onClick={() => setAction('Sign Up')}>Sign Up</div>
+          <div className={action === "Log In" ? "submit grey" : "submit"} onClick={() => setAction('Log In')}>Log In</div>
         </div>
     </div>
   )
