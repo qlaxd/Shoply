42badcf945647499b1705caa9d837a96ef710df5
levdev0 <bsze3lajlev@vasvari.org>
2024-10-29 22:28:10 +0100

    useState hook implemented, edited auth.service.js for handling login/registration requests

--- Forráskód/Web/web-app/src/components/LoginSignup/LoginSignup.js
+++ Forráskód/Web/web-app/src/components/LoginSignup/LoginSignup.js
@@ -1,44 +1,111 @@
-import React, {useState} from 'react'
-import './LoginSignup.css'
+import React, {useState} from 'react';
+import './LoginSignup.css';
+import AuthService from '../../services/auth.service.js';
 
-// importing png icons
-import user_icon from '../assets/user.png'
-import email_icon from '../assets/email.png'
-import password_icon from '../assets/padlock.png'
+import user_icon from '../assets/user.png';
+import email_icon from '../assets/email.png';
+import password_icon from '../assets/padlock.png';
 
-// LoginSignup komponens
 const LoginSignup = () => {
+  const [action, setAction] = useState('Sign Up');
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
 
-  const [action,setAction] = useState('Sign Up') // melyik gomb az aktív - action változó
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      if (action === 'Sign Up') {
+        await AuthService.register(formData.username, formData.email, formData.password);
+        alert('Sikeres regisztráció!');
+      } else {
+        await AuthService.login(formData.email, formData.password);
+        alert('Sikeres bejelentkezés!');
+      }
+    } catch (error) {
+      alert(error.message || 'Hiba történt!');
+    }
+  };
 
   return (
     <div className='container'>
-        <div className='header'>
-          <div className='text'>{action}</div>
-          <div className='underline'></div>
-        </div>
-        <div className='inputs'>
-          {action === "Log In" ? <div></div>:<div className='input'>
+      <div className='header'>
+        <div className='text'>{action}</div>
+        <div className='underline'></div>
+      </div>
+      <div className='inputs'>
+        {action === "Log In" ? <div></div> : 
+          <div className='input'>
             <img src={user_icon} alt='user' />
-            <input type='text' placeholder='Username' />
-          </div>}
-          <div className='input'>
-            <img src={email_icon} alt='email' />
-            <input type='email' placeholder='Email' />
+            <input 
+              type='text' 
+              placeholder='Username'
+              name='username'
+              value={formData.username}
+              onChange={handleChange}
+            />
           </div>
-          <div className='input'>
-            <img src={password_icon} alt='password' />
-            <input type='password' placeholder='Password' />
-          </div>
+        }
+        <div className='input'>
+          <img src={email_icon} alt='email' />
+          <input 
+            type='email' 
+            placeholder='Email'
+            name='email'
+            value={formData.email}
+            onChange={handleChange}
+          />
         </div>
-        {/* ha a Sign Up action az aktív akkor nincs forgot password, ha Log In akkor van */}
-        {action === "Sign Up" ? <div></div>:<div className='forgot-password'>Forgot Password? <span>Click Here</span></div>} 
-        <div className='submit-container'>
-          <div className={action === "Sign Up" ? "submit grey" : "submit"} onClick={() => setAction('Sign Up')}>Sign Up</div>
-          <div className={action === "Log In" ? "submit grey" : "submit"} onClick={() => setAction('Log In')}>Log In</div>
+        <div className='input'>
+          <img src={password_icon} alt='password' />
+          <input 
+            type='password' 
+            placeholder='Password'
+            name='password'
+            value={formData.password}
+            onChange={handleChange}
+          />
         </div>
+      </div>
+      {action === "Sign Up" ? <div></div> : 
+        <div className='forgot-password'>Forgot Password? <span>Click Here</span></div>
+      }
+      <div className='submit-container'>
+        <div 
+          className={action === "Sign Up" ? "submit grey" : "submit"} 
+          onClick={() => {
+            if (action !== "Sign Up") {
+              setAction("Sign Up");
+            } else {
+              handleSubmit();
+            }
+          }}
+        >
+          Sign Up
+        </div>
+        <div 
+          className={action === "Log In" ? "submit grey" : "submit"} 
+          onClick={() => {
+            if (action !== "Log In") {
+              setAction("Log In");
+            } else {
+              handleSubmit();
+            }
+          }}
+        >
+          Log In
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
--- Forráskód/Web/web-app/src/services/api.js
+++ Forráskód/Web/web-app/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
 
-dotenv.config();
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
